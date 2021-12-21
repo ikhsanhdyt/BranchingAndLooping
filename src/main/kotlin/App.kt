e5e0e41429fbda98a304ceb26b3a69fca895917d
ikhsanhdyt <ikhsnhdyt@gmail.com>
2021-12-21 21:05:04 +0700

    Initial commit

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,44 @@
+import java.lang.NumberFormatException
+
+class App {
+    var studentScore: Int = 0
+    fun run() {
+        printHeader()
+        inputStudentScore()
+        checkStudentScore(studentScore)
+    }
+
+    fun printHeader() {
+        println("=====================")
+        println("Student Exam Score Checker")
+        println("=====================")
+    }
+
+    fun inputStudentScore() {
+        println("Enter Student Score = ")
+        studentScore = try {
+            readLine()?.toInt() ?:0
+        }catch (e: NumberFormatException){
+            0
+        }
+    }
+
+    fun checkStudentScore(studentScore: Int) {
+        if (studentScore < 0 || studentScore > 100) {
+            println("Input Failed, Please re Input")
+            inputStudentScore()
+            checkStudentScore(this.studentScore)
+        }else if (studentScore > 75) {
+            println("Student Pass")
+        }else{
+            println("Student Failed")
+        }
+    }
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val app = App()
+            app.run()
+        }
+    }
+}
